afbf546f7e82b4a2c12d55674111b48bacc17365
Martin Ghiena <josemartinghiena@gmail.com>
2020-08-27 10:45:43 -0300

    Reordenando

--- news_scraper/news_scraper.py
+++ news_scraper/news_scraper.py
@@ -16,6 +16,43 @@ is_well_formed_url= re.compile(r'^https?://.+/.+$') # i.e. https://www.somesite.
 is_root_path = re.compile(r'^/.+$') # i.e. /some-text
 logger = logging.getLogger(__name__)
 
+def _save_articles(news_site_uid, articles):
+    now = datetime.datetime.now()
+    csv_headers = list(filter(lambda property: not property.startswith('_'), dir(articles[0])))
+    out_file_name = '{news_site_uid}_{datetime}_articles.csv'.format(news_site_uid=news_site_uid, datetime=now.strftime('%Y_%m_%d'))
+
+    with open(out_file_name, mode='w+') as f:
+        writer = csv.writer(f)
+        writer.writerow(csv_headers)
+
+        for article in articles:
+            row = [str(getattr(article, prop)) for prop in csv_headers]
+            writer.writerow(row)
+
+def _build_link(host, link):
+    if is_well_formed_url.match(link):
+        return link
+    elif is_root_path.match(link):
+        return '{host}{uri}'.format(host=host, uri=link)
+    else:
+        return '{host}/{uri}'.format(host=host, uri=link)
+
+
+def _fetch_article(news_site_uid, host, link):
+    logger.info('Start fetching article at {}'.format(link))
+
+    article = None
+    try:
+        article = news.ArticlePage(news_site_uid, _build_link(host, link))
+    except (HTTPError, MaxRetryError) as e:
+        logger.warning('Error while fetching article!', exc_info=False)
+
+    if article and not article.body:
+        logger.warning('Invalid article. There is no body.')
+        return None
+
+    return article
+
 
 def _news_scraper(news_site_uid):
     host = config()['news_sites'][news_site_uid]['url']
@@ -37,43 +74,6 @@ def _news_scraper(news_site_uid):
     _save_articles(news_site_uid, articles)
 
 
-def _fetch_article(news_site_uid, host, link):
-    logger.info('Start fetching article at {}'.format(link))
-
-    article = None
-    try:
-        article = news.ArticlePage(news_site_uid, _build_link(host, link))
-    except (HTTPError, MaxRetryError) as e:
-        logger.warning('Error while fetching article!', exc_info=False)
-
-    if article and not article.body:
-        logger.warning('Invalid article. There is no body.')
-        return None
-
-    return article
-
-
-def _build_link(host, link):
-    if is_well_formed_url.match(link):
-        return link
-    elif is_root_path.match(link):
-        return '{host}{uri}'.format(host=host, uri=link)
-    else:
-        return '{host}/{uri}'.format(host=host, uri=link)
-
-
-def _save_articles(news_site_uid, articles):
-    now = datetime.datetime.now()
-    csv_headers = list(filter(lambda property: not property.startswith('_'), dir(articles[0])))
-    out_file_name = '{news_site_uid}_{datetime}_articles.csv'.format(news_site_uid=news_site_uid, datetime=now.strftime('%Y_%m_%d'))
-
-    with open(out_file_name, mode='w+') as f:
-        writer = csv.writer(f)
-        writer.writerow(csv_headers)
-
-        for article in articles:
-            row = [str(getattr(article, prop)) for prop in csv_headers]
-            writer.writerow(row)
 
 
 if __name__ == '__main__':
